8ec69699d386f07d9ceafbed4bfa9227c81a91f4
carnae <samarbaskar02@gmail.com>
2024-10-01 23:58:08 +0530

    refactor main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 a = [1, 4, 6, 3, 9, 12, 20]
 
-search = int(input("Enter number"))
-for i in a:
-    if i == search:
-        print("Found")
+x = int(input("enter number"))
+if x in a:
+    print("found")
+else:
+    print("not found")
