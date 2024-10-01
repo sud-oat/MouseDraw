a02712fb99db1c00c280a4bdcc46a03dba34b5b8
Sudyotman Sudershan <sudyot.s@gmail.com>
2024-10-02 00:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ array = load_image(r"image\pinguin.png")
 with open('image.txt', 'w') as file:
     for row in array:
         for pixel in row:
-            print("Lol")
+            divider = 255/2
+            if pixel>=divider:
+                file.write(1)
+            else:
+                file.write(0)
 
         file.write("\n")
 
